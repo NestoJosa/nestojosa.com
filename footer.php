7f71b0f32dc5990ad4d3a3802746e4336924f842
Ernesto Hinojosa <eh@ernesto.one>
2021-02-07 15:10:33 +0100

    Add to base
    - Add generic markup to header, front-page and footer

---
+++ footer.php
@@ -0,0 +1,16 @@
+    </div>
+  <!-- /PageWrapper -->
+
+  <!-- footer -->
+    <footer>
+      <!-- wp_footer() -->
+        <?php wp_footer(); ?>
+      <!-- /wp_footer() -->
+    </footer>
+  <!-- /footer -->
+
+</body>
+</html>
+
+  
+
---
+++ header.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="<?php bloginfo( 'language' ); ?>">
+<head>
+
+  <meta charset="<?php bloginfo( 'charset' ); ?>">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- remove default iOS styling of tel: links -->
+  <meta name="format-detection" content="telephone=no">
+  <!-- <link rel="shortcut icon" type="image/png" href="<?php // echo get_template_directory_uri();?>/dist/img/favicon.png" /> -->
+
+  <?php wp_head(); ?>
+
+</head>
+<body>
+  <!-- wp_body_open() -->
+    <?php wp_body_open(); ?>
+  <!-- /wp_body_open() -->
+
+  <!-- header -->
+    <header>
+    </header>
+  <!-- /header -->
+
+  <!-- PagerWrapper -->
+    <div class="PageWrapper">
