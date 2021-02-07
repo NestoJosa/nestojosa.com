083019ea9b0d9cbc7f4148adb386bcd099c753c1
Ernesto Hinojosa <eh@ernesto.one>
2021-02-07 19:11:36 +0100

    Add component: PageWrapper

--- front-page.php
+++ front-page.php
@@ -1,4 +1,13 @@
 <?php get_header(); ?>
 
+  <div class="container-fluid">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-8">
+          <h1>Hello, world</h1>
+        </div>
+      </div>
+    </div>
+  </div>
 
 <?php get_footer(); ?>
